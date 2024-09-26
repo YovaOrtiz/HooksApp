3ae5afb6017ada1dce24691372ee0325ab67d1eb
Jovani Ortiz Barraza <yourgitemail@example.com>
2024-09-25 21:24:53 -0700

    Refactor: Update components to display Pokémon information and loading state

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -1,17 +1,46 @@
 import React from 'react';
-import { useFetch } from '../hooks'; // Asegúrate de que la ruta sea correcta
+import { useCounter, useFetch } from '../hooks'; // Asegúrate de que la ruta sea correcta
+import { LoadingMessage } from './LoadingMessage';
+import { PokemonCard } from './PokemonCard';
 
 export const MultipleCustomHooks = () => {
-  const { data, hasError, isLoading } = useFetch('https://pokeapi.co/api/v2/pokemon/3');
+  const {counter, decrement, increment} = useCounter(1);  
+  const { data, hasError, isLoading } = useFetch(`https://pokeapi.co/api/v2/pokemon/${counter}`);
 
   return (
     <>
       <h1>Información de Pokemon</h1>
       <hr />
 
-      {isLoading && <p>Cargando...</p>}
+      {isLoading 
+      ? <LoadingMessage/>
+      :(
+      <PokemonCard  
+      id={counter}
+       name={data.name}
+       sprites={[
+        data.sprites.front_default,
+        data.sprites.front_shiny,
+        data.sprites.back_default,
+        data.sprites.back_shiny,
+       ]}
+       />)
+    }
       
-      <h2>{data?.name}</h2>
+    
+
+      <button
+      className='btn btn-primary mt-2'
+      onClick={() => counter > 1 ? decrement() : null}
+      >
+        Anterior
+      </button>
+      <button
+      className='btn btn-primary mt-2'
+      onClick={() => increment()}
+      >
+        Siguiente
+      </button>
     </>
   );
 };
